034d5e54a475d3edf09dcb4a0725dfed4c331a52
sujiu <2994338151@qq.com>
2024-11-02 22:01:51 +0800

    2024.11.2

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <el-aside width="200px">
+        <el-menu>
+            <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <span>aaa</span>
+            </el-menu-item>
+            <el-sub-menu index="1">
+                <template #title>
+                    <el-icon>
+                        <location />
+                    </el-icon>
+                    <span>Navigator One</span>
+                </template>
+                <el-menu-item index="1-1">item one</el-menu-item>
+            </el-sub-menu>
+
+        </el-menu>
+
+
+    </el-aside>
+
+</template>
+
+<style lang="scss" scoped>
+.el-aside {
+    height: 100vh;
+}
+</style>
--- src/views/MainBox.vue
+++ src/views/MainBox.vue
@@ -1,13 +1,21 @@
 <template>
-    <div>
-        <div>navbar</div>
-        <div>sidebar</div>
-        <router-view></router-view>
-    </div>
+
+
+    <el-container>
+        <SideMenu />
+        <el-container direction="vertical">
+            <TopHeader />
+            <el-main>
+                <routerView>
+                </routerView>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script setup>
-
+import TopHeader from '@/components/TopHeader.vue';
+import SideMenu from '@/components/SideMenu.vue';
 </script>
 
 <style lang="scss" scoped></style>
