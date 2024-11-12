1846ce2e9f4544f437f7803809bb3535bc27c8a2
sujiu <2994338151@qq.com>
2024-11-12 11:25:09 +0800

    2024.11.12

---
+++ src/components/upload/Upload.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-upload
+    class="avatar-uploader"
+    :on-change="handleChange"
+    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+    :show-file-list="false"
+    :auto-upload="false"
+  >
+    <img v-if="userForm.avatar" :src="init + userForm.avatar" class="avatar" />
+    <el-icon v-else class="avatar-uploader-icon">
+      <Plus />
+    </el-icon>
+  </el-upload>
+</template>
+
+<script setup>
+import { defineEmits, defineProps } from "vue";
+
+const handleChange = (file) => {
+  // console.log(file);
+  userForm.avatar = URL.createObjectURL(file.raw);
+  // console.log(userForm.avatar);
+
+  // console.log(file.raw);
+  userForm.file = file.raw;
+  // userForm.avatar = URL.createObjectURL(file);
+  // userForm.file = file;
+
+  // 检查是否存在 raw 属性，如果没有直接使用 file
+
+  init.value = "";
+};
+</script>
--- src/views/MainBox.vue
+++ src/views/MainBox.vue
@@ -1,21 +1,18 @@
 <template>
-
-
-    <el-container>
-        <SideMenu />
-        <el-container direction="vertical">
-            <TopHeader />
-            <el-main>
-                <routerView>
-                </routerView>
-            </el-main>
-        </el-container>
+  <el-container>
+    <SideMenu />
+    <el-container direction="vertical">
+      <TopHeader />
+      <el-main>
+        <routerView> </routerView>
+      </el-main>
     </el-container>
+  </el-container>
 </template>
 
 <script setup>
-import TopHeader from '@/components/TopHeader.vue';
-import SideMenu from '@/components/SideMenu.vue';
+import TopHeader from "@/components/mainbox/TopHeader.vue";
+import SideMenu from "@/components/mainbox/SideMenu.vue";
 </script>
 
 <style lang="scss" scoped></style>
